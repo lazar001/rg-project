b6f040d82c665723394d2aebc004bfa1ac0a0e4b
lazar001 <102490358+lazar001@users.noreply.github.com>
2022-04-08 17:18:20 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -497,7 +497,7 @@ int main() {
 
         // render
         // ------
-        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //render scene into floating point framebuffer
